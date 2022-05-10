801258b401e7a8f949ad7b8d877fe01344100446
alessandrokonrad <akonrad6@gmail.com>
2022-05-10 17:48:14 +0200

    removed border

--- src/parts/spacebud/utils.ts
+++ src/parts/spacebud/utils.ts
@@ -1,5 +1,3 @@
-import { theme } from "../../theme/theme";
-
 export const downloadPFP = async (budId: number, url: string) => {
   const download = (dataurl: string, filename: string) => {
     const link = document.createElement("a");
@@ -20,17 +18,16 @@ export const downloadPFP = async (budId: number, url: string) => {
   canvas.width = 1500;
 
   const ctx = canvas.getContext("2d");
-  ctx.translate(-341, -162);
+  ctx.translate(-341, -163);
   // not relevant right now, but in case we want a border around the image we can do it with this arc
-  ctx.beginPath();
-  ctx.fillStyle = theme.colors.primary;
-  ctx.arc(784, 606, 443, 0, Math.PI * 2);
-  ctx.fill();
+  // ctx.beginPath();
+  // ctx.fillStyle = theme.colors.primary;
+  // ctx.arc(784, 606, 443, 0, Math.PI * 2);
+  // ctx.fill();
   ctx.beginPath();
   ctx.arc(784, 606, 443, 0, Math.PI * 2);
   ctx.clip();
   const img = await loadImage(url);
-  console.log(img);
   ctx.drawImage(img, 0, 0, 1500, 1500);
 
   const tempCanvas = document.createElement("canvas"),
